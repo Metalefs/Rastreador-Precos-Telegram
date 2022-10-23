7006a078850ad080acc87731ac11f31a0c08d354
Jackson Ramalho <jackson.pires.rm@gmail.com>
2022-10-23 10:48:41 -0300

    perf: remove puppeteer from search

--- src/bot/services/productEnrichment.service.ts
+++ src/bot/services/productEnrichment.service.ts
@@ -15,7 +15,7 @@ export class ProductEnrichmentService {
 
   async enrich(product: Product, chatId?) {
     const result = await this.priceFinder.getPrices(product.name);
-
+    console.log({result})
     if(!result.link){
       console.error('could not find offers for '+product.name)
       return;
